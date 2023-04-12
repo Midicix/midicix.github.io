6354abe6b01f721c2bf4e9b2964a06ad9e6fb477
Midicix <94143634+Midicix@users.noreply.github.com>
2023-04-13 00:58:16 +0200

    Ajout de la page "calendrier"

--- src/js/script.js
+++ src/js/script.js
@@ -107,9 +107,63 @@ function getHour(){
     setTimeout(getHour, 1000);
 }
 
-function getYear(){
-    contenu = document.getElementById("year");
+function calendar() {
     dateFull = new Date();
+    date = String(dateFull.getDate());
+    month = String(dateFull.getMonth() + 1);
+    contenu = document.getElementById("year");
+    
     year = dateFull.getFullYear();
     contenu.innerText = year;
+
+    if (date.length == 1)
+    {
+        d0 = '0';
+        d1 = date[0];
+    }
+    else
+    {
+        d0 = date[0];
+        d1 = date[1];
+    }
+    realDate = d0 + d1;
+
+    if (month.length == 1)
+    {
+        mo0 = '0';
+        mo1 = month[0];
+    }
+    else
+    {
+        mo0 = month[0];
+        mo1 = month[1];
+    }
+    realMonth = mo0 + mo1;
+
+    jour = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+    mois = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+
+    prefixJ = "date";
+    prefixM = "month";
+
+
+    for (i = 0; i < jour.length ; i++){
+        if (jour[i] == realDate){
+            document.getElementById(prefixJ + jour[i]).style.backgroundColor = "#25E0C9";
+        }
+        else{
+            document.getElementById(prefixJ + jour[i]).style.backgroundColor = "#3B3838";
+        }
+    }
+
+    for (i = 0; i < mois.length ; i++){
+        if (mois[i] == realMonth){
+            document.getElementById(prefixM + mois[i]).style.backgroundColor = "#288371";
+        }
+        else{
+            document.getElementById(prefixM + mois[i]).style.backgroundColor = "#3B3838";
+        }
+    }
+    
+    setTimeout(calendar, 1000);
 }
